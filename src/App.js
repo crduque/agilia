bcce00d40c6e927ee52afb7630cd9a956b22fb06
Cristina Rodríguez <crduque@hotmail.com>
2021-05-27 10:47:38 +0200

    listing the names of the first page of the api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
+  useEffect(()=>{
+    fetch("https://reqres.in/api/users", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(responseJson => {
+				setUsers(responseJson.data);
+			})
+			.catch(error => {
+				setError(error);
+			});
+	}, []);
+
+  let usersName = users.map((user, index) => {
+    return (
+      <li>{user.first_name}</li>
+    )
+  })
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +36,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {users ? usersName : ""}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
